71c0ae1aaa21e79c0d5a4fa09be0e5e15f51b0c2
endurance21 <43696525+endurance21@users.noreply.github.com>
2019-05-23 05:51:50 +0530

    update
    chnaged code indentation and removed unneccassary things

--- script.js
+++ script.js
@@ -1,46 +1,19 @@
  
 var currentindex=1;
-
-
 function  next()
 {    
-    
-    currentindex++;     
-    if(currentindex==6){
-        currentindex=1;   
-}   
-                      
-                     
-                       document.querySelector("#down").setAttribute('href','#div'+ (currentindex));
-                       console.log(currentindex);
-                       document.getElementById('down').click();
-                       
-                       
-
-
-
-                 
+  currentindex++;     
+  if(currentindex==6)
+     currentindex=1;            
+  document.querySelector("#down").setAttribute('href','#div'+ (currentindex));
+  document.getElementById('down').click();                
 }
-
-
-
-
 
 function prev()
 {       
     currentindex--;
     if(currentindex==0)
-    {                      currentindex=5;
-   } 
-
-
-              
-             document.querySelector("#up").setAttribute('href','#div'+ (currentindex) );
-             document.getElementById('up').click();
-             console.log(currentindex);
-             
-
-            
-          
-
+       currentindex=5;
+  document.querySelector("#up").setAttribute('href','#div'+ (currentindex) );
+  document.getElementById('up').click();
 }
